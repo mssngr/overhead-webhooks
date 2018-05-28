93644d3e2d8039bc4d49689521b8f96e1d9d1230
mssngr <gabriel.konkle@gmail.com>
2018-05-28 16:35:55 -0600

    Update webhook

--- dropboxWebhook.js
+++ dropboxWebhook.js
@@ -1,4 +1,8 @@
 module.exports = function (context, cb) {
+  const Dropbox = require('dropbox').Dropbox
+  new Dropbox({ accessToken: context.secrets.dropboxAccessToken })
+    .filesListFolder({path: 'Overhead'})
+    .then(console.log, console.error);
   console.log(context.data.list_folder)
   cb(null, 'done')
 }
